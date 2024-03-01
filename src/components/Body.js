1fa2c0fd39fb002453bd488b84ce31e8242c9711
Jatin <jatinchanana001@gmail.com>
2024-03-01 23:20:51 +0530

    css changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,7 +5,7 @@ import Shimmer from "./Shimmer";
 import Card from "./WeatherCard";
 
 const DEFAULT_CITY = "Delhi";
-const DAY_WISE_INDEX = [5, 13, 22, 27, 35];
+const DAY_WISE_INDEX = [4, 12, 22, 27, 35];
 const Body = () => {
   const [city, setcity] = useState(DEFAULT_CITY);
   const [inputCity, setInputCity] = useState(DEFAULT_CITY);
